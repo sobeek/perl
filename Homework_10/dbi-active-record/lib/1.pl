1c01f4812306c80ccdc00b13e821795db52bafb7
Pavel Sobyanin <sobeek@yandex.ru>
2017-05-11 17:09:08 +0300

    hw 10 fixes

--- Homework_10/dbi-active-record/lib/1.pl
+++ Homework_10/dbi-active-record/lib/1.pl
@@ -2,16 +2,9 @@
 use Local::MusicLib::Album;
 use Local::MusicLib::Track;
 use Local::MusicLib::Artist;
-use Local::MusicLib::DB::SQLite;
-use DBI::ActiveRecord::DB;
-use DBI::ActiveRecord;
 use strict;
 use warnings;
 use DDP;
-
-#print db 'muslib.db';
-
-my $x = DBI::ActiveRecord::Object->new();
 
 =head1 track testing
 my $track = Local::MusicLib::Track->new(id => 1, name => "x", extension => ".mp3", duration => "00:02:13", create_time => DateTime->new(year => 2014, month => 02, day => 02), album_id => 1);
@@ -38,7 +31,7 @@ p $album->select("name", "x");
 
 my $artist = Local::MusicLib::Artist->new(id => 1, name => "e", country => "ru", create_time => DateTime->new(year => 2014, month => 02, day => 02));
 
-#$artist->insert();
+$artist->insert();
 #$artist->delete();
 #$artist->update();
 #p $artist->select("name", "z");
